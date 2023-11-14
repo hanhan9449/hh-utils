2faed42381e03ef0363a4f5df0b602c7a95de7ac
hanhan <haannn@qq.com>
2023-11-14 21:22:28 +0800

    feat: init

---
+++ pure/mergeClasses.ts
@@ -0,0 +1,21 @@
+
+
+export function mergeClasses(...ss: (string | false | undefined)[]): string;
+export function mergeClasses() {
+    let result = ''
+    const n = arguments.length
+    let needSpace = false
+    for (let i = 0; i < n; i++) {
+        // eslint-disable-next-line prefer-rest-params
+        if (arguments[i]) {
+            if (needSpace) {
+                result += ' '
+                needSpace = false
+            }
+            // eslint-disable-next-line prefer-rest-params
+            result += arguments[i]
+            needSpace = true
+        }
+    }
+    return result
+}
